dd2432db2c14992dddd07c256c7659f4f0d257d3
Lacy Zhang <lacy.zhang@hotmail.com>
2021-05-11 22:54:14 +1000

    ticket controller for fatch tickets/ ticket

---
+++ lib/controllers/ticket_controller.rb
@@ -0,0 +1,23 @@
+require_relative '../gateways/ticket_gateway'
+require_relative '../views/tickets_view'
+require_relative '../views/ticket_view'
+
+class TicketController
+  def initialize(email, token)
+    @tickets_view = TicketsView.new
+    @ticket_view = TicketView.new
+    @ticket_gateway = TicketGateway.new(email, token)
+    @prompt = TTY::Prompt.new
+  end
+
+  def tickets(page_number)
+    response = @ticket_gateway.fetch("https://lacyzhang.zendesk.com/api/v2/tickets.json?page=#{page_number}&per_page=25")
+    @tickets_view.display(response['tickets'])
+    response['next_page']
+  end
+
+  def ticket(ticket_number)
+    response = @ticket_gateway.fetch("https://lacyzhang.zendesk.com/api/v2/tickets/#{ticket_number}.json")
+    @ticket_view.display(response['ticket'])
+  end
+end
